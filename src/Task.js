e4a3b467bc2126b6345993dfcd373e02adb6032b
Afiq Akram <81454701+afiqakraam@users.noreply.github.com>
2023-06-25 09:39:02 +0700

    Add files via upload

---
+++ src/Task.js
@@ -0,0 +1,5 @@
+const url = "YOURDEPLOYURL" // TODO: replace this
+const studentName = "Afiq Akram" // TODO: replace this
+const studentId = "FE5144619" // TODO: replace this
+
+export { url, studentName, studentId }
